0ea3bb3dd22556c5fafcbc132b8a0825a67d5989
yarmolovich <slonimpb@gmail.com>
2019-08-26 17:23:54 +0300

    Конфигаруционный ini файл и подключение к базе

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
     require_once ('exdbase.php');
 
-    
+    $ini_cfg = parse_ini_file("config.ini", true );
 
     // Создаем экземпляр класа для работы с MySQL
     $DB = new exDBase($ini_cfg['DB_HOST'], $ini_cfg['$DB_USER'], $ini_cfg['DB_PASSWORD'], $ini_cfg['DB_NAME']);
